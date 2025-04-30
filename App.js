06f3fe2cbb551e02b643f23f5c1a35e505f38fe4
Edgardo Crespo Lindeman <edgcrespo@uade.edu.ar>
2025-04-29 21:12:03 -0300

    Interaccion con Android

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Counter from './src/Counter'; // Importa el componente Counter
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Counter />       
       <StatusBar style="auto" />
     </View>
   );
