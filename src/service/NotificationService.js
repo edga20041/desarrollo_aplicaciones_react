22428b0f0648b0748e512888acf0c2e62630741a
totossj2 <loferrario@uade.edu.ar>
2025-06-28 19:33:54 -0300

    bugfix notification service

--- src/service/NotificationService.js
+++ src/service/NotificationService.js
@@ -1,8 +1,8 @@
 // src/services/NotificationService.js
-import * as Notifications from 'expo-notifications';
-import axios from '../utils/axiosInstance';
+import * as Notifications from "expo-notifications";
+import axios from "../axiosInstance";
 
-const API_BASE = '/novedades';
+const API_BASE = "/novedades";
 let notificationInterval = null;
 let lastSeenTimestamp = 0;
 
@@ -18,7 +18,7 @@ export async function configureNotifications() {
 
 export async function requestNotificationPermissions() {
   const { status } = await Notifications.requestPermissionsAsync();
-  return status === 'granted';
+  return status === "granted";
 }
 
 export async function sendNotification(title, body) {
@@ -31,15 +31,14 @@ export async function sendNotification(title, body) {
 export async function startPeriodicNotifications(intervalMinutes = 1) {
   if (notificationInterval) clearInterval(notificationInterval);
 
-
   try {
     const resInit = await axios.get(`${API_BASE}/ultima`);
     lastSeenTimestamp = resInit.data;
   } catch (err) {
-    console.warn('[Notification] init timestamp falló', err);
+    console.warn("[Notification] init timestamp falló", err);
   }
 
-//Se debe estructurar una API en el backend, consultado si hay pediddos nuevos y que end point si da true dispare notificacion y si da false no haga nada
+  //Se debe estructurar una API en el backend, consultado si hay pediddos nuevos y que end point si da true dispare notificacion y si da false no haga nada
   const intervalMs = intervalMinutes * 60 * 1000;
   notificationInterval = setInterval(async () => {
     try {
@@ -48,17 +47,19 @@ export async function startPeriodicNotifications(intervalMinutes = 1) {
 
       // 2) si hay novedades, traigo detalles y notifico
       if (serverTs > lastSeenTimestamp) {
-        const { data: nuevos } = await axios.get(`${API_BASE}?since=${lastSeenTimestamp}`);
+        const { data: nuevos } = await axios.get(
+          `${API_BASE}?since=${lastSeenTimestamp}`
+        );
         if (nuevos.length > 0) {
           await sendNotification(
-            '¡Tienes nuevos pedidos!',
+            "¡Tienes nuevos pedidos!",
             `Hay ${nuevos.length} entrega(s) por despachar.`
           );
         }
         lastSeenTimestamp = serverTs;
       }
     } catch (error) {
-      console.error('[Notification] Error en polling:', error);
+      console.error("[Notification] Error en polling:", error);
     }
   }, intervalMs);
 
